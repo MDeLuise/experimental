8a374ee5fb85c1a68dabcfc31085fef7f88d5671
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2022-09-02 15:27:38 +0200

    [spring-security] Basic Auth with: open endpoints, predefined user

--- spring-security/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,14 +1,29 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -21,5 +36,17 @@ public class ApplicationSecurityConfig {
             .httpBasic()
             .and()
             .build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails maxUserBuilder = User.builder()
+            .username("max")
+            .password(passwordEncoder.encode("max"))
+            .roles("student")
+            .build();
+
+        return new InMemoryUserDetailsManager(maxUserBuilder);
     }
 }
